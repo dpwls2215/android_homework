b021da58994a6cdeaaf13f2dea5aca33a87f4922
dpwls2215 <dpwls2215@naver.com>
2018-11-26 16:56:11 +0900

    first

--- app/src/main/java/com/example/jiwon/myapplication/HistoryActivity.java
+++ app/src/main/java/com/example/jiwon/myapplication/HistoryActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HistoryActivity extends AppCompatActivity {
-
+	
+	public double a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
